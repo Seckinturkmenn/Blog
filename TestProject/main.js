e01d44e0fa3c01840c00152b1dc648d13f84004d
Seckinturkmenn <76449041+Seckinturkmenn@users.noreply.github.com>
2021-01-01 12:59:33 +0300

    Add files via upload

--- TestProject/main.js
+++ TestProject/main.js
@@ -1,5 +1,8 @@
 var contentData = {};
 var headerData = {};
+var blueIcon = {};
+var greenIcon = {};
+var redIcon = {};
 
 $(document).ready(function () {
     compilePage();
@@ -21,42 +24,51 @@ function compilePage(){
 
     
     $('#table_id').DataTable();
-    
-        $.ajax({
-            url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson",
-            type: "GET",
-            async: false,
-            success: function (data) {
-                contentData.earthquake = data.features;
-            },
-            error: console.log("Deprem verisi alınamadı!")
-        });
-    
-        $.ajax({
-            url: "Content/header.html",
-            type: "GET",
-            async: false,
-            success: function (header) {
-                $("#m_header").html(Handlebars.compile(header)(headerData));
-            },
-            error: console.log("Sayfa compile edilemedi.")
-        });
-    
-        $.ajax({
-            url: "Content/mapPage.html",
-            type: "GET",
-            async: false,
-            success: function (mapPage) {
-                $("#m_content").html(Handlebars.compile(mapPage)(contentData));
-            },
-            error: console.log("Sayfa compile edilemedi.")
-        });
+    $.ajax({
+        url: "https://api.orhanaydogdu.com.tr/deprem/live.php?limit=50",
+        type: "GET",
+        async: false,
+        success: function (data) {
+            contentData.kandilli = data.result;
+        },
+        error: console.log("Deprem verisi alınamadı!")
+    });
 
-        CreateMap();
+    $.ajax({
+        url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson",
+        type: "GET",
+        async: false,
+        success: function (data) {
+            contentData.earthquake = data.features;
+        },
+        error: console.log("Deprem verisi alınamadı!")
+    });
+
+    $.ajax({
+        url: "Content/header.html",
+        type: "GET",
+        async: false,
+        success: function (header) {
+            $("#m_header").html(Handlebars.compile(header)(headerData));
+        },
+        error: console.log("Sayfa compile edilemedi.")
+    });
+    getEarthIconCounts();
+    getKandilliIconCounts();
+    $.ajax({
+        url: "Content/mapPage.html",
+        type: "GET",
+        async: false,
+        success: function (mapPage) {
+            $("#m_content").html(Handlebars.compile(mapPage)(contentData));
+        },
+        error: console.log("Sayfa compile edilemedi.")
+    });
+    CreateMap();
 }
 
 function CreateMap(){
-    var greenIcon = new L.Icon({
+    greenIcon = new L.Icon({
         iconUrl: 'images/marker-icon-2x-green.png',
         shadowUrl: 'images/marker-shadow.png',
         iconSize: [25, 41],
@@ -65,7 +77,7 @@ function CreateMap(){
         shadowSize: [41, 41]
     });
 
-    var redIcon = new L.Icon({
+    redIcon = new L.Icon({
         iconUrl: 'images/marker-icon-2x-red.png',
         shadowUrl: 'images/marker-shadow.png',
         iconSize: [25, 41],
@@ -74,7 +86,7 @@ function CreateMap(){
         shadowSize: [41, 41]
     });
 
-    var blueIcon = new L.Icon({
+    blueIcon = new L.Icon({
         iconUrl: 'images/marker-icon-2x-blue.png',
         shadowUrl: 'images/marker-shadow.png',
         iconSize: [25, 41],
@@ -82,9 +94,40 @@ function CreateMap(){
         popupAnchor: [1, -34],
         shadowSize: [41, 41]
     });
+    createEarthquakeMap();
+    createKandilliMap();
+}
 
+function createKandilliMap(){
+    var map = L.map('mapKandilli', {
+        center: [contentData.kandilli[0].lat, contentData.kandilli[0].lng],
+        zoom: 6
+    });
+
+    //L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png', {
+        attribution: ' &copy; <a href="https://www.seckinturkmen.com/" target="_blank">OpenStreetMap</a> katılımcıları'
+    }).addTo(map);   
+
+    for(var i=0; i < contentData.kandilli.length; i++) {
+        if(contentData.kandilli[i].mag <= 3)
+        {
+            L.marker([contentData.kandilli[i].lat, contentData.kandilli[i].lng], { icon: blueIcon }).addTo(map).bindPopup(contentData.kandilli[i].lokasyon + "<br> Tarih: " + contentData.kandilli[i].date);
+        }
+        else if(contentData.kandilli[i].mag > 3 && contentData.kandilli[i].mag <= 6)
+        {
+            L.marker([contentData.kandilli[i].lat, contentData.kandilli[i].lng], { icon: greenIcon }).addTo(map).bindPopup(contentData.kandilli[i].lokasyon + "<br> Tarih: " + contentData.kandilli[i].date);
+        }  
+        else
+        {
+            L.marker([contentData.kandilli[i].lat, contentData.kandilli[i].lng], { icon: redIcon }).addTo(map).bindPopup(contentData.kandilli[i].lokasyon + "<br> Tarih: " + contentData.kandilli[i].date); 
+        }   
+    }
+}
+
+function createEarthquakeMap(){
     var map = L.map('map', {
-        center: [25.7865, -80.194],
+        center: [contentData.earthquake[0].geometry.coordinates[1], contentData.earthquake[0].geometry.coordinates[0]],
         zoom: 6
     });
 
@@ -105,6 +148,48 @@ function CreateMap(){
         else
         {
             L.marker([contentData.earthquake[i].geometry.coordinates[1], contentData.earthquake[i].geometry.coordinates[0]], { icon: redIcon }).addTo(map).bindPopup(contentData.earthquake[i].properties.place); 
+        }   
+    }
+}
+
+function getKandilliIconCounts(){
+    contentData.kandilliBlueIconCount = 0;
+    contentData.kandilliRedIconCount = 0;
+    contentData.kandilliGreenIconCount = 0;
+
+    for(var i=0; i < contentData.kandilli.length; i++) {
+        if(contentData.kandilli[i].mag <= 3)
+        {
+            contentData.kandilliBlueIconCount++;
+        }
+        else if(contentData.kandilli[i].mag > 3 && contentData.kandilli[i].mag <= 6)
+        {
+            contentData.kandilliGreenIconCount++;
+        }  
+        else
+        {
+            contentData.kandilliRedIconCount++;
+        }   
+    }
+}
+
+function getEarthIconCounts(){
+    contentData.blueIconCount = 0;
+    contentData.redIconCount = 0;
+    contentData.greenIconCount = 0;
+
+    for(var i=0; i < contentData.earthquake.length; i++) {
+        if(contentData.earthquake[i].properties.mag <= 3)
+        {
+            contentData.blueIconCount++;
+        }
+        else if(contentData.earthquake[i].properties.mag > 3 && contentData.earthquake[i].properties.mag <= 6)
+        {
+            contentData.greenIconCount++;
+        }  
+        else
+        {
+            contentData.redIconCount++;
         }   
     }
 }
